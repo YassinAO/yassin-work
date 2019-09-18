1e64c943235bc7271d1c4a38a0275e9b1074e2a7
Y4SSIN <yassin_developer@outlook.com>
2019-09-18 13:00:35 +0200

    Create necesarry models

---
+++ app/Project.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    protected $fillable = ['title', 'description', 'body'];
+}
