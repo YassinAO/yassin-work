0d93007d5d8615a7241082c6f70223539373fbf2
Y4SSIN <yassin_developer@outlook.com>
2019-09-18 12:57:50 +0200

    Create neccesary views

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+Route::get('/dashboard/posts', 'DashboardController@post');
+Route::get('/dashboard/projects', 'DashboardController@project');
+Route::get('/dashboard/careers', 'DashboardController@career');
+
+Auth::routes([ 'register' => false ]);
+
+Route::resource('projects', 'ProjectController');
+Route::resource('posts', 'PostController');
+Route::resource('careers', 'CareerController');
+
+Route::get('/category/{id}', 'PostController@category')->name('category');
+Route::resource('categories', 'CategoryController', ['except' => ['create']]);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
