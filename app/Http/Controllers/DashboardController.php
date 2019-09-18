5d2d9d6c3d547a7c5aa32753740bab49c624892a
Y4SSIN <yassin_developer@outlook.com>
2019-09-18 13:01:08 +0200

    Create necesarry controllers

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use App\Post;
+use App\Project;
+use App\Career;
+
+class DashboardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {   
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+        $posts = Post::orderBy('id', 'desc')->paginate(5);
+        return view('dashboard.index')->with('posts', $user->posts)->with('posts', $posts);
+    }
+
+    public function post(){
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+        $posts = Post::orderBy('id', 'desc')->paginate(5);
+        return view('dashboard.posts')->with('posts', $user->posts)->with('posts', $posts);
+    }
+
+    public function project(){
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+        $projects = Project::orderBy('id', 'desc')->paginate(5);
+        return view('dashboard.projects')->with('projects', $user->projects)->with('projects', $projects);
+    }
+
+    public function career(){
+        $user_id = auth()->user()->id;
+        $user = User::find($user_id);
+        $careers = Career::orderBy('id', 'desc')->paginate(5);
+        return view('dashboard.careers')->with('careers', $user->careers)->with('careers', $careers);
+    }
+}
